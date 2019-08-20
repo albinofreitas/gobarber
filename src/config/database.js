43674331da17bed43335ac3125e5ddae544bfe22
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-20 01:28:28 -0300

    add sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postegres',
+  password: '123456',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    undesrcored: true,
+    undesrcoredAll: true,
+  },
+};
