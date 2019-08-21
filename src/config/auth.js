1a8f4c8ae6d11fe36f108f983432fc2e783a3a2e
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-20 22:15:09 -0300

    Authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '48d91c05-ddd8-4b05-bebc-bcabe76d16aa',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import AuthController from './app/controllers/AuthController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/auth', AuthController.store);
 
 export default routes;
