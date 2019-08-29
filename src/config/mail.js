2a5a59c56ed998edd1fc892b4d990896eb213aa5
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-28 23:30:22 -0300

    Config nodemailer with mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fe14e9db063ca8',
+    pass: '235b56de105652',
+  },
+  default: {
+    from: 'Equipe gobaber <noreplay@gobarber.com>',
+  },
+};
